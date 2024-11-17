bfae8ca8b4ce07c35c486b65d146c183f0f56b93
CABLord <146165295+CABLord@users.noreply.github.com>
2024-11-17 20:23:20 +0100

    Update use-toast.ts

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -1,13 +1,5 @@
-
 "use client"
 
 import { useToast as useToastOriginal } from "@/components/ui/toast"
 
-// If useToastOriginal is undefined, provide a dummy implementation
-const dummyToast = () => ({
-  toast: () => {},
-  dismiss: () => {},
-  // Add any other methods that the original useToast might have
-});
-
-export const useToast = useToastOriginal || dummyToast;
+export const useToast = useToastOriginal
